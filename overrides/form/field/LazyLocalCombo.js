fe9306e60acc9c73efe7e7e77dfaf01d0c8462af
Bill Clinton <BillClinton@users.noreply.github.com>
2015-06-29 00:13:28 -0400

    removed unneeded requirement for this version of Ext JS

--- overrides/form/field/LazyLocalCombo.js
+++ overrides/form/field/LazyLocalCombo.js
@@ -1,6 +1,5 @@
 Ext.define('Jarvus.ext.override.form.field.LazyLocalCombo', {
     override: 'Ext.form.field.ComboBox',
-    requires: ['Jarvus.ext.override.data.StoreIsLoaded'],
 
     lazyAutoLoad: true,
 
@@ -8,15 +7,15 @@ Ext.define('Jarvus.ext.override.form.field.LazyLocalCombo', {
         this.doLazyLoad();
         this.callParent(arguments);
     },
-    
+
     setValue: function(value) {
         this.callParent(arguments);
-        
+
         if (!Ext.isEmpty(value)) {
             this.doLazyLoad();
         }
     },
-    
+
     doLazyLoad: function() {
         var me = this,
             store = me.getStore();
