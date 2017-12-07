463f414adfc98689e0a78c9b31bd554fa34c4dd9
Chris Alfano <chris@jarv.us>
2017-12-06 21:58:36 -0500

    Defer value actions until store loads

--- overrides/form/field/LazyLocalCombo.js
+++ overrides/form/field/LazyLocalCombo.js
@@ -3,25 +3,47 @@ Ext.define('Jarvus.override.form.field.LazyLocalCombo', {
 
     lazyAutoLoad: true,
 
-    doQuery: function() {
-        this.doLazyLoad();
-        this.callParent(arguments);
+    doQuery: function doQuery() {
+        var me = this,
+            args = arguments,
+            previous = args.callee.$previous;
+
+        this.doLazyLoad(function() {
+            previous.apply(me, args);
+        });
     },
 
     setValue: function(value) {
-        this.callParent(arguments);
+        var me = this,
+            args = arguments,
+            previous = args.callee.$previous;
 
-        if (!Ext.isEmpty(value)) {
-            this.doLazyLoad();
+        if (Ext.isEmpty(value)) {
+            previous.apply(me, args);
+        } else {
+            this.doLazyLoad(function() {
+                previous.apply(me, args);
+            });
         }
     },
 
-    doLazyLoad: function() {
+    doLazyLoad: function(callback) {
         var me = this,
-            store = me.getStore();
+            store = me.getStore(),
+            isLoading = store.isLoading(),
+            rawValue = me.getRawValue(),
+            onLoad = function() {
+                me.setRawValue(rawValue);
+                Ext.callback(callback);
+            };
 
-        if (me.queryMode == 'local' && me.lazyAutoLoad && !store.isLoading() && !store.isLoaded()) {
-            store.load();
+        if (me.queryMode == 'local' && me.lazyAutoLoad && !isLoading && !store.isLoaded()) {
+            me.expand();
+            store.load({ callback: onLoad });
+        } else if (isLoading) {
+            store.on('load', onLoad, me, { single: true });
+        } else {
+            Ext.callback(callback);
         }
     }
 });
